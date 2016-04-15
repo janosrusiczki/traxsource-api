384cd548a96b599c9db6cb684d85d40250d2b685
Janos Rusiczki <janos.rusiczki@gmail.com>
2016-04-15 07:09:21 -0400

    Thin server listening on 0.0.0.0:2500

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge(Host: '0.0.0.0', Port: 2500)
+    end
+  end
+end
